d09923ecba73f20ad9a9dd3442347d764cb7b965
Mohamed Taman <mohamed.taman@devtechgroup.com>
2019-10-03 16:57:04 +0200

    Add automatic versioning to services

--- service1/src/main/java/rs/com/siriusxi/devtech/example/flywayjpa/customer/api/CustomerController.java
+++ service1/src/main/java/rs/com/siriusxi/devtech/example/flywayjpa/customer/api/CustomerController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 import rs.com.siriusxi.devtech.example.flywayjpa.customer.domain.Customer;
 import rs.com.siriusxi.devtech.example.flywayjpa.customer.service.CustomerService;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.time.Instant;
 import java.util.List;
 
@@ -15,14 +19,39 @@ public class CustomerController {
 
   private CustomerService customerService;
 
-    public CustomerController(CustomerService customerService) {
-        this.customerService = customerService;
-    }
+  public CustomerController(CustomerService customerService) {
+    this.customerService = customerService;
+  }
 
-    @GetMapping("health")
-    public String health(){
-        return "(Service 1) is Up & Running, ".concat(Instant.now().toString());
+  @GetMapping("version")
+  public String versionInformation() {
+    return readGitProperties();
+  }
+  private String readGitProperties() {
+    ClassLoader classLoader = getClass().getClassLoader();
+    try (InputStream inputStream = classLoader.getResourceAsStream("git.properties")){
+      return readFromInputStream(inputStream);
+    } catch (IOException e) {
+      e.printStackTrace();
+      return "Version information could not be retrieved";
     }
+  }
+  private String readFromInputStream(InputStream inputStream)
+          throws IOException {
+    StringBuilder resultStringBuilder = new StringBuilder();
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+      String line;
+      while ((line = br.readLine()) != null) {
+        resultStringBuilder.append(line).append("\n");
+      }
+    }
+    return resultStringBuilder.toString();
+  }
+
+  @GetMapping("health")
+  public String health() {
+    return "(Service 1) is Up & Running, ".concat(Instant.now().toString());
+  }
 
   @GetMapping("customers")
   public List<Customer> getCustomerService() {
