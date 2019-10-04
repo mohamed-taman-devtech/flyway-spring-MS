30778840dac506b22907cbb9b8d971b35b9bcf48
Mohamed Taman <mohamed.taman@devtechgroup.com>
2019-10-04 12:47:50 +0200

    testing

--- service1/src/main/resources/db/migration/V1__Create_Intial_Schema.sql
+++ service1/src/main/resources/db/migration/V1__Create_Intial_Schema.sql
@@ -1,3 +1,8 @@
+-- Create schema is required because we are using MySQL 5.7
+-- Flyway with MySQL version 8 creates the schema by default
+create SCHEMA customer_service;
+USE customer_service;
+
 create table user1(
     id bigint not null auto_increment,
     name varchar(255) not null unique,
