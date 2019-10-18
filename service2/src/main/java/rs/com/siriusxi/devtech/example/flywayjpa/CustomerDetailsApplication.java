328a93cbb63b29326dba71ffddcff639acbdc139
Mohamed Taman <mohamed.taman@devtechgroup.com>
2019-10-18 16:59:01 +0200

    Add versioning and Distributed tracing

--- service2/src/main/java/rs/com/siriusxi/devtech/example/flywayjpa/CustomerDetailsApplication.java
+++ service2/src/main/java/rs/com/siriusxi/devtech/example/flywayjpa/CustomerDetailsApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Service2Application {
+public class CustomerDetailsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Service2Application.class, args);
+		SpringApplication.run(CustomerDetailsApplication.class, args);
 	}
 }
