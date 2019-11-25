e58fd9f1dfb59e841cbc84a8928b6b3875e237f2
Mohamed Taman <mohamed.taman@devtechgroup.com>
2019-11-25 14:59:51 +0100

    Add hazelcast Cache solution

--- service1/src/main/java/rs/com/siriusxi/devtech/example/flywayjpa/customer/api/CustomerController.java
+++ service1/src/main/java/rs/com/siriusxi/devtech/example/flywayjpa/customer/api/CustomerController.java
@@ -1,10 +1,8 @@
 package rs.com.siriusxi.devtech.example.flywayjpa.customer.api;
 
+import com.hazelcast.core.HazelcastInstance;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import rs.com.siriusxi.devtech.example.flywayjpa.customer.domain.Customer;
 import rs.com.siriusxi.devtech.example.flywayjpa.customer.service.CustomerService;
@@ -15,22 +13,27 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.time.Instant;
 import java.util.List;
+import java.util.Map;
 
 @RestController()
-@RequestMapping("api/customers")
+@RequestMapping("api")
 @Log4j2
 public class CustomerController {
 
   private final CustomerService customerService;
   private final RestTemplate restTemplate;
+  private final HazelcastInstance instance;
 
-  public CustomerController(CustomerService customerService, RestTemplate restTemplate) {
+  public CustomerController(CustomerService customerService,
+                            RestTemplate restTemplate,
+                            HazelcastInstance instance) {
+
     this.customerService = customerService;
     this.restTemplate = restTemplate;
+    this.instance = instance;
   }
 
   @GetMapping("version")
-  @Cacheable("version")
   public String versionInformation() {
     return readGitProperties();
   }
@@ -61,13 +64,61 @@ public class CustomerController {
     return "(Service 1) is Up & Running, ".concat(Instant.now().toString());
   }
 
-  @GetMapping
+  @GetMapping("customers/details")
   public List<Customer> getCustomerService() {
     String customerDetails =
-        restTemplate.getForObject("http://localhost:8080/api/customer" + "/details", String.class);
+            restTemplate.getForObject("http://localhost:8080/api/customer" + "/details", String.class);
 
     log.info("Received data from Customer details service {}", customerDetails);
 
     return customerService.getCustomers();
   }
+
+// Hazelcast Demo
+
+  @GetMapping("customers/add")
+  public String addCustomer(@RequestParam("key") String key,
+                            @RequestParam("value") String value) {
+
+    instance.getMap("customers")    // get map from hazelcast cluster
+            // write value, This value will be accessible from another
+            // jvm also
+            .put(key, value);
+
+    return "Customer:" + value + " has been written to the Cache";
+  }
+
+  @GetMapping("customers/{key}")
+  public String readCustomer(@PathVariable("key") String key) {
+    // get map from hazelcast, also notice it casted to a normal Java Map
+    Map<String, String> customersMap = instance.getMap("customers");
+
+    // read customer value
+    return "Customer from Hazelcast cache is :" + customersMap.get(key);
+  }
+
+  @GetMapping("customers")
+  public Map<String, String> getAllCustomers() {
+    return instance.getMap("customers");
+  }
+
+
+  // Another Hazelcast config cache
+  @GetMapping("config/write")
+  public String writeConfig(@RequestParam("values") String values) {
+    instance.getMap("Configs")
+            .put("data", values);
+
+    return "Configurations values has been written to Hazelcast";
+  }
+
+  @GetMapping("config")
+  public String readConfig() {
+    return "Configurations from Hazelcast is : "
+            + instance
+            .getMap("Configs")
+            .get("data");
+  }
+
+
 }
